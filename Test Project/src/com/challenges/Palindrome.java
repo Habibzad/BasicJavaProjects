a6dc36409f613003d386360e77fa2d89d728c6b6
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-01-25 11:44:52 -0500

    Overload the isPalindrome method to also check if a string is palindrome

--- Test Project/src/com/challenges/Palindrome.java
+++ Test Project/src/com/challenges/Palindrome.java
@@ -2,13 +2,31 @@ package com.challenges;
 
 public class Palindrome {
 	
-	public static boolean isPalindrome(int num) {
+	public static String isPalindrome(int num) {
 		String numValue = String.valueOf(num);
-		return numValue.equals(new StringBuilder(numValue).reverse().toString());
+		if(numValue.equals(new StringBuilder(numValue).reverse().toString())) {
+			return "is Palindrome";
+		}
+		else {
+			return "is not Palindrome";
+		}
+		 
+	}
+	
+	public static String isPalindrome(String str) {
+		String reversedStr = new StringBuffer(str).reverse().toString();
+		if(str.equals(reversedStr)) {
+			return "is Palindrome";
+		}
+		else {
+			return "is not Palindrome";
+		}
+		 
 	}
 
 	public static void main(String[] args) {
 		System.out.println(isPalindrome(123454321));
+		System.out.println(isPalindrome("rotor"));
 	}
 
 }
